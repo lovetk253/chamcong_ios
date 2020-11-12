3c849b1aa50cf589a65fa2b5e69becc4c869eb22
nguyen <nguyen@nguyens-iMac.local>
2020-11-12 01:40:52 -0800

    up code

--- chamcong/ViewController.swift
+++ chamcong/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let url = URL(string: "https://chamcong.tsg.net.vn")
+        let urlRequest = URLRequest(url: url!)
+        
+        webView.loadRequest(urlRequest)
     }
 
 
